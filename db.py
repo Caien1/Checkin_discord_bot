d8c4e78e91caf750e161403e8dd5aa721bd9d8ca
Caien1 <pulwama01@gmail.com>
2024-12-20 02:17:29 +0530

    !checkin function complete

---
+++ db.py
@@ -0,0 +1,119 @@
+
+import sqlite3
+from datetime import datetime
+
+
+def create_table(cursor):
+    create_table_query = """
+                    CREATE TABLE IF NOT EXISTS users (
+                    user_id INTEGER PRIMARY KEY,
+                    points INTEGER DEFAULT 0 ,
+                    last_checkin DATETIME DEFAULT CURRENT_TIMESTAMP
+                    )
+                    """
+    cursor.execute(create_table_query)
+    pass
+
+
+def check_if_user_exists(cursor,user_id):
+   
+    user_checker_query = """
+                SELECT EXISTS(
+                SELECT 1 FROM users
+                WHERE
+                user_id =?
+                )
+    """
+
+    cursor.execute(user_checker_query,[user_id])
+    result=cursor.fetchone()[0]
+    return result==1
+    pass
+    
+def create_user_and_update_points(cursor,user_id):
+
+    create_user_query = """
+                    INSERT INTO users(
+                    user_id,points,last_checkin
+                    )
+                     values(
+                     ?,?,CURRENT_TIMESTAMP
+                     )
+                     """
+    cursor.execute(create_user_query,[user_id,10])
+
+
+def update_points(cursor,user_id):
+    update_points_query = """
+                            UPDATE users
+                            SET points = points + ? , last_checkin = CURRENT_TIMESTAMP
+                            WHERE
+                            user_id = ?
+    """
+    cursor.execute(update_points_query,[10,user_id])
+
+def disply_users(cursor):
+    query = "SELECT * FROM users"
+    cursor.execute(query)
+    return cursor.fetchall()
+
+def can_checkin(cursor,user_id):
+    last_checkin_query = """
+    SELECT last_checkin from users
+    WHERE user_id = ?
+
+    """
+    cursor.execute(last_checkin_query,[user_id])
+   
+    time_at_last_checkin = datetime.strptime(cursor.fetchone()[0],"%Y-%m-%d %H:%M:%S")
+    time_delta= datetime.now()-time_at_last_checkin
+
+    return time_delta.days>=1
+
+
+
+def create_db_connection():
+    connection = sqlite3.connect('users.db');
+    cursor = connection.cursor()
+    return connection,cursor
+
+
+
+def close_connection(connection):
+    connection.commit()
+    connection.close()    
+
+
+
+
+def main(user_id):
+    tranction_success = False
+    did_exist = True
+    connection,cursor = create_db_connection()
+    try:
+        cursor.execute("BEGIN TRANSACTION")
+        exists=check_if_user_exists(cursor,user_id)
+
+        if not exists:
+            create_user_and_update_points(cursor,user_id)
+            did_exist = False
+            tranction_success=True
+
+        elif can_checkin(cursor,user_id):
+            update_points(cursor,user_id)
+            tranction_success =True
+
+        
+
+        connection.commit()
+
+        
+
+    except sqlite3.Error as e:
+        connection.rollback()
+        print(f"Some thing went wrong: {e}")
+    
+    finally:
+        close_connection(connection)
+        return tranction_success,did_exist
+    
---
+++ init.py
@@ -0,0 +1,51 @@
+import discord
+from dotenv import load_dotenv
+from db import *
+import os
+
+ 
+
+load_dotenv()
+token = os.getenv("TOKEN");
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+client =discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('!checkin'):
+
+        success,did_exist=main(message.author.id)
+        connection, cursor = create_db_connection();
+        print(disply_users(cursor))
+        close_connection(connection)
+
+        if success and did_exist:
+            await message.channel.send(f"Thanks for the Checkin {message.author}")
+        elif success:
+            await message.channel.send(f"Welcome to the family perform daily checking to win points {message.author}")
+        else:
+            await message.channel.send(f"Please try again later you alredy have done your checkin {message.author}")
+
+
+             
+
+
+    if message.content.startswith('!forest'):
+        await message.channel.send(f"This will load an image in the future")
+    
+    if message.content.startswith('!leaderboard'):
+                await message.channel.send(f"This will load an image in the future")
+
+client.run(token)
