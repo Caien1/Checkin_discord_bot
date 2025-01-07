f4809c616598369addde4e7ea204528160f412c8
Caien1 <pulwama01@gmail.com>
2025-01-07 17:36:04 +0530

    added server

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from forest_gerator import *
 import os
 import datetime
 from http.server import *
+from flask import Flask
  
 
 load_dotenv()
@@ -13,38 +14,19 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 
+app = Flask(__name__)
 client =discord.Client(intents=intents)
+port = int(os.getenv('PORT', 4000))
+app.run(host='0.0.0.0', port=port, debug=True) 
 
-  
-# creating a class for handling  
-# basic Get and Post Requests 
-class GFG(BaseHTTPRequestHandler): 
-    
-    # creating a function for Get Request 
-    def do_GET(self): 
-        
-        # Success Response --> 200 
-        self.send_response(200) 
-          
-        # Type of file that we are using for creating our 
-        # web server. 
-        self.send_header('content-type', 'text/html') 
-        self.end_headers() 
-          
-        # what we write in this function it gets visible on our 
-        # web-server 
-        self.wfile.write('<h1>GFG - (GeeksForGeeks)</h1>'.encode()) 
-  
-  
-# this is the object which take port  
-# number and the server-name 
-# for running the server 
-port = HTTPServer(('', 5555), GFG) 
-  
-# this is used for running our  
-# server as long as we wish 
-# i.e. forever 
-port.serve_forever() 
+
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
 
 
 @client.event
